aa2cbda2181bb36a2e7e9d99ca5fd33ba6bede18
kennvil102 <kennvilchez@gmail.com>
2024-06-27 14:40:39 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Rutas from './routes/Rutas'
 
 function App() {
-  const [count, setCount] = useState(0)
+   
 
   return (
     <>
-      
+      <Rutas/>
     </>
   )
 }
